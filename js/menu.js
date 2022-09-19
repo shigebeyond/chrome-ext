b84f022ecbb918c29355ca40af9384a0d9b9db91
shijianhang <3076679995@qq.com>
2022-09-19 18:29:06 +0800

    完善代码

--- js/menu.js
+++ js/menu.js
@@ -24,9 +24,9 @@ chrome.contextMenus.create({
 });
 
 
-// 2 网页剪切报
+// 2 网页剪报
 chrome.contextMenus.create({
-    title: '网页剪切报',
+    title: '网页剪报',
     id: '2',//一级菜单的id
     contexts: ['page', 'selection'], // page表示页面右键就会有这个菜单，如果想要当选中文字时才会出现此右键菜单，用：selection
     onclick: function (params) {
@@ -37,9 +37,124 @@ chrome.contextMenus.create({
             let txt = params['selectionText'];
             if(typeof(txt) != "undefined")
                 note = note  + "\n" + txt;
+            TINY.box.show(note,0,0,0,0,3);
             $.post("http://localhost/note.php",{note: note},function(result){
-                alert(result);
+                // Ealt.Etoast(result, 2)
+                // chrome.tabs.executeScript(tab.id, {
+                //     code: "Ealt.Etoast('" + result + "', 3);"
+                // });
             });
         });
     }
 });
+
+
+
+// 3 有道词典
+// 从 https://youdao.com/ 扒下来的api
+chrome.contextMenus.create({
+    title: '有道词典',
+    id: '3',//一级菜单的id
+    contexts: ['selection'], // page表示页面右键就会有这个菜单，如果想要当选中文字时才会出现此右键菜单，用：selection
+    onclick: function (params) {
+        chrome.tabs.getSelected(null, function (tab) {
+            let txt = params['selectionText'];
+            if(typeof(txt) == "undefined"){
+                return
+            }
+            // api
+            $.get("https://dict.youdao.com/suggest?num=5&ver=3.0&doctype=json&cache=false&le=en&q=" + txt,function(result,status){
+                let entries = result['data']['entries'];
+                let msg = '词典释义:';
+                for (let entry of entries){
+                    msg += "\n" + entry['entry'] + ': ' + entry['explain'];
+                }
+                msg += "\n\n是否打词典网页？" 
+                if (confirm(msg)) {
+                    // 网页
+                    window.open("https://youdao.com/result?word=" + txt + "&lang=en", "有道翻译", "");
+                }
+            });
+        });
+    }
+});
+
+// 4 有道翻译
+// 从 https://fanyi.youdao.com/ 扒下来的api
+chrome.contextMenus.create({
+    title: '有道翻译',
+    id: '4',//一级菜单的id
+    contexts: ['selection'], // page表示页面右键就会有这个菜单，如果想要当选中文字时才会出现此右键菜单，用：selection
+    onclick: function (params) {
+        chrome.tabs.getSelected(null, function (tab) {
+            let txt = params['selectionText'];
+            if(typeof(txt) == "undefined"){
+                return
+            }
+            let r = generateSaltSign(txt);
+            let headers = {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+                Referer: 'https://fanyi.youdao.com/',
+                Host: 'fanyi.youdao.com',
+                Origin: 'https://fanyi.youdao.com',
+                'Cache-Control': 'no-cache',
+                Connection: 'keep-alive',
+            };
+            let data = {
+                i: txt,
+                from: "AUTO",
+                to: "AUTO",
+                smartresult: "dict",
+                client: "fanyideskweb",
+                salt: r.salt,
+                sign: r.sign,
+                lts: r.ts,
+                bv: r.bv,
+                doctype: "json",
+                version: "2.1",
+                keyfrom: "fanyi.web",
+                action: "FY_BY_REALTlME"
+            };
+            // url是 /translate_o -- https://www.cnblogs.com/lpyy/p/14399676.htm
+            //let url = "https://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"; // 奇怪，网站调试js是这个url，但响应错误{"errorCode":50}, 看上面文章是差不多，只不过多加了cookie
+            // url 是 /translate -- https://zhuanlan.zhihu.com/p/368198711
+            let url = "https://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule";
+            m = $.ajax({
+                type: "POST",
+                contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+                url: url,
+                headers: headers,
+                data: data,
+                dataType: "json",
+                success: function(result) {
+                    //alert(JSON.stringify(result));
+                    let entries = result['translateResult'][0];
+                    let msg = '逐段翻译:';
+                    for (let entry of entries){
+                        // src原文，tgt翻译
+                        msg += "\n" + entry['tgt'];
+                    }
+                    alert(msg);
+                },
+                error: function(e) {
+                    alert('异常: ' + JSON.stringify(e));
+                }
+            })
+        });
+    }
+});
+
+// 有道翻译-生成签名
+function generateSaltSign(txt) {
+    // let navigatorAppVersion = '5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36';
+    // let ver = $.md5(navigatorAppVersion);
+    let ver = '84afaa1f696d79a939ece6748a3b1fcd';
+    let ts = "" + (new Date).getTime()
+      , salt = ts + parseInt(10 * Math.random(), 10);
+    return {
+        ts: ts,
+        bv: ver,
+        salt: salt,
+        sign: $.md5("fanyideskweb" + txt + salt + "Ygy_4c=r#e#4EX^NUGUc5")
+    }
+}
