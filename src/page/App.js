a7c9d64bdc3752da860967ce8942051b2495667e
shijianhang <772910474@qq.com>
2022-10-10 23:55:00 +0800

    devtool

--- src/page/App.js
+++ src/page/App.js
@@ -3,14 +3,14 @@ import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 // import './App.css';
 import TabList from './TabList'
-import OptionForm from './OptionForm'
+import HttpPanel from './HttpPanel'
 
 // 路由参考 https://github.com/remix-run/react-router/blob/dev/examples/basic/src/App.tsx
 export default function App() {
   return (
     <Routes>
       <Route path="/tabList" element={<TabList />} />
-      <Route path="/optionForm" element={<OptionForm />} />
+      <Route path="/httpPanel" element={<HttpPanel />} />
       <Route path="*" element={<Index />} />
     </Routes>
   );
@@ -22,10 +22,10 @@ function Index() {
     <div>
       <h2>入口</h2>
       <p>
-        <Link to="/tabList">跳转到 TabList 页</Link>
+        <Link to="/tabList">跳转到: 备份标签页管理</Link>
       </p>
       <p>
-        <Link to="/optionForm">跳转到 options 页</Link>
+        <Link to="/httpPanel">跳转到: 选项配置</Link>
       </p>
     </div>
   );
